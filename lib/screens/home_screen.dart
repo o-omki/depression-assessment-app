672a0b1622027725d4353d5ab3b8deca5033d584
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2023-04-28 19:45:40 +0530

    Create a blank home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,15 +4,18 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
+    return const Center(
+      child: Text(
+        "Home Screen",
+        style: TextStyle(fontSize: 40),
       ),
     );
   }
