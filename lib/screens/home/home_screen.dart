a5d4cf7f6393bc68f1b3eb4acc2422bb37f25e56
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2023-05-07 23:09:14 +0530

    Add card for emergency helpline

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,6 +4,8 @@ import '../../main.dart';
 import '../../widget/chart_graph.dart';
 import 'questionnaire.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,6 +16,16 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String phoneNumber = '+919999666555';
+  void _launchPhone() async {
+    final Uri url = Uri.parse('tel:$phoneNumber');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -82,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
           thickness: 2,
         ),
         SizedBox(
-          height: mq.height * .02,
+          height: mq.height * .05,
         ),
         Card(
           elevation: 4,
@@ -91,11 +103,12 @@ class _HomeScreenState extends State<HomeScreen> {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding:
+                const EdgeInsets.only(left: 10, top: 10, bottom: 10, right: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   'Show up for the Questionnaire',
                   style: TextStyle(fontSize: 21, fontWeight: FontWeight.w600),
                 ),
@@ -103,27 +116,86 @@ class _HomeScreenState extends State<HomeScreen> {
                   height: mq.height * .02,
                 ),
                 Align(
-                    alignment: Alignment.topRight,
-                    child: ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const Questionnaire()));
-                        },
-                        icon: Icon(
-                          Icons.run_circle_rounded,
-                          size: 30,
-                        ),
-                        label: SizedBox(
-                          width: mq.width * .2,
-                          child: Text(
-                            'Start',
-                            style: TextStyle(fontSize: 21),
-                            textAlign: TextAlign.center,
-                          ),
-                        ))),
+                  alignment: Alignment.topRight,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Questionnaire()));
+                    },
+                    icon: const Icon(
+                      Icons.run_circle_rounded,
+                      size: 30,
+                    ),
+                    label: SizedBox(
+                      width: mq.width * .2,
+                      child: const Text(
+                        'Start',
+                        style: TextStyle(fontSize: 21),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
               ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: mq.height * .05,
+        ),
+        GestureDetector(
+          onTap: _launchPhone,
+          child: Card(
+            elevation: 4,
+            color: Colors.blue.shade300,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Emergency Helpline',
+                    style: TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: mq.height * .02,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/customer-care.png',
+                        height: mq.height * 0.05,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: mq.width * 0.05,
+                      ),
+                      const Text(
+                        '+91 9999 666 555',
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: mq.height * .02,
+                  ),
+                  const Align(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      'Vandrevala Foundation',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
