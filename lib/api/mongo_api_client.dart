0b9b80b5d7491547c670d022ce2dc3df9150bc11
o_omki <omkarmurty7@gmail.com>
2023-05-08 22:45:06 +0530

    Incorporated dotenv.

--- lib/api/mongo_api_client.dart
+++ lib/api/mongo_api_client.dart
@@ -1,12 +1,16 @@
 import 'package:mongo_dart/mongo_dart.dart';
+import "package:flutter_dotenv/flutter_dotenv.dart";
+
 
 class MongoDbApiClient {
+  static final mongoUri = dotenv.env['MONGODB_URI']!;
   static Future<List<Map<String, dynamic>>> getAllEntities(String collectionName) async {
-    final db = await Db.create('mongodb+srv://o_omki:test1234@serenity-cluster.qlqtek2.mongodb.net/SERENITY_SPACE?retryWrites=true&w=majority');
+    final db = await Db.create(mongoUri);
     await db.open();
 
   
     final entityCollection = db.collection(collectionName);
+    
     final entities = await entityCollection.find().toList();
     
     await db.close();
@@ -15,7 +19,7 @@ class MongoDbApiClient {
   }
   
   static Future<Map<String, dynamic>> getEntityById(String collectionName, ObjectId id) async {
-    final db = await Db.create('mongodb+srv://o_omki:test1234@serenity-cluster.qlqtek2.mongodb.net/SERENITY_SPACE?retryWrites=true&w=majority');
+    final db = await Db.create(mongoUri);
     await db.open();
   
     final entityCollection = db.collection(collectionName);
@@ -27,7 +31,7 @@ class MongoDbApiClient {
   }
   
   static Future<void> addEntity(String collectionName, Map<String, dynamic> document) async {
-    final db = await Db.create('mongodb+srv://o_omki:test1234@serenity-cluster.qlqtek2.mongodb.net/SERENITY_SPACE?retryWrites=true&w=majority');
+    final db = await Db.create(mongoUri);
     await db.open();
   
     final entityCollection = db.collection(collectionName);
@@ -37,7 +41,7 @@ class MongoDbApiClient {
   }
   
   static Future<void> updateCounsellor(String id, Map<String, dynamic> updatedCounsellor) async {
-    final db = await Db.create('mongodb+srv://o_omki:test1234@serenity-cluster.qlqtek2.mongodb.net/SERENITY_SPACE?retryWrites=true&w=majority');
+    final db = await Db.create(mongoUri);
     await db.open();
   
     final entityCollection = db.collection('doctors_master');
@@ -47,7 +51,7 @@ class MongoDbApiClient {
   }
   
   static Future<void> deleteCounsellor(String id) async {
-    final db = await Db.create('mongodb+srv://o_omki:test1234@serenity-cluster.qlqtek2.mongodb.net/SERENITY_SPACE?retryWrites=true&w=majority');
+    final db = await Db.create(mongoUri);
     await db.open();
   
     final entityCollection = db.collection('doctors_master');
@@ -55,4 +59,37 @@ class MongoDbApiClient {
     
     await db.close();
   }
+
+
+  static Future<Map<String, dynamic>> getUserAppointments(String userId) async {
+  final db = await Db.create(mongoUri);
+    await db.open();
+  
+  final appointmentsCollection = db.collection('appointments_master');
+  
+  final completedAppointments = await appointmentsCollection
+      .find(where.eq('user_id', userId)
+      .eq('status', 'Completed'))
+      .toList();
+  
+  final upcomingAppointments = await appointmentsCollection
+      .find(where.eq('user_id', userId)
+      .eq('status', 'Upcoming'))
+      .toList();
+  
+  final cancelledAppointments = await appointmentsCollection
+      .find(where.eq('user_id', userId)
+      .eq('status', 'Cancelled'))
+      .toList();
+  
+  await db.close();
+  
+  final userAppointments = {
+    "completed": completedAppointments,
+    "upcoming": upcomingAppointments,
+    "cancelled": cancelledAppointments
+  };
+  
+  return userAppointments;
+}
 }
