9ab14fef89ab9b3e8f8513d2e3f034262b01f50a
o_omki <omkarmurty7@gmail.com>
2023-05-12 23:11:02 +0530

    Add Serializable journal model class.

---
+++ lib/models/journal_model.dart
@@ -0,0 +1,39 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'journal_model.g.dart';
+
+@JsonSerializable()
+class JournalModel {
+  @JsonKey(name: '_id')
+  String id;
+
+  List<JournalEntry> journalEntries;
+
+  JournalModel({
+    required this.id,
+    required this.journalEntries,
+  });
+
+  factory JournalModel.fromJson(Map<String, dynamic> json) =>
+      _$JournalModelFromJson(json);
+  Map<String, dynamic> toJson() => _$JournalModelToJson(this);
+}
+
+@JsonSerializable()
+class JournalEntry {
+  DateTime date;
+  String description;
+
+  @JsonKey(name: 'mood_value')
+  String moodValue;
+
+  JournalEntry({
+    required this.date,
+    required this.description,
+    required this.moodValue,
+  });
+
+  factory JournalEntry.fromJson(Map<String, dynamic> json) =>
+      _$JournalEntryFromJson(json);
+  Map<String, dynamic> toJson() => _$JournalEntryToJson(this);
+}
