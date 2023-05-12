e175a454c29e36255385ba14992d3ef4089d2eb9
o_omki <omkarmurty7@gmail.com>
2023-05-12 23:13:18 +0530

    Add json serializable generated files.

---
+++ lib/models/journal_model.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'journal_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+JournalModel _$JournalModelFromJson(Map<String, dynamic> json) => JournalModel(
+      id: json['_id'] as String,
+      journalEntries: (json['journalEntries'] as List<dynamic>)
+          .map((e) => JournalEntry.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$JournalModelToJson(JournalModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'journalEntries': instance.journalEntries,
+    };
+
+JournalEntry _$JournalEntryFromJson(Map<String, dynamic> json) => JournalEntry(
+      date: DateTime.parse(json['date'] as String),
+      description: json['description'] as String,
+      moodValue: json['mood_value'] as String,
+    );
+
+Map<String, dynamic> _$JournalEntryToJson(JournalEntry instance) =>
+    <String, dynamic>{
+      'date': instance.date.toIso8601String(),
+      'description': instance.description,
+      'mood_value': instance.moodValue,
+    };
